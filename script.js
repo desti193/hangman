a24a70ea6397f6cce0237282c609f002cd26f83f
charlesofili05 <ifechukwudeofili04@gmail.com>
2023-06-17 16:37:33 +0100

    added new game

--- script.js
+++ script.js
@@ -1,48 +1,76 @@
-//selecting the screen 
+//selecting the screen
 let screen = document.querySelector(".textarea");
-
-let keys = document.querySelectorAll(".btn")
-
-//creating the word that will be displayed
-let word = "Hang Man"
-
+let newGameBtn = document.querySelector(".newgame");
+let keys = document.querySelectorAll(".btn");
+let word = "Hang Man";
 let letters = word.toUpperCase().split("");
+//creating the word that will be displayed
+let words = [
+  "Foot Ball",
+  "Basket Ball",
+  "Hockey",
 
-const displayWord = ()=>{
-    letters.forEach(char => {
-        if (char == ' ') {
-            let space = document.createElement("div")
-            space.classList.add("space");
-            screen.appendChild(space);
+  "Brain",
+  "Elbow",
+  "Toe Nail",
 
-        }else{
-            let letter = document.createElement("input")
-            letter.type = "text"
-            letter.classList.add("letter");
-            letter.value = char;
-            letter.classList.add("invisible")
-            let letterBox = document.createElement("div");
-            letterBox.classList.add("letter-box")
-            letterBox.appendChild(letter);
-            screen.appendChild(letterBox)
-        }
-    });
-}
-
-const checkWord = (letter)=>{
-    let answer = document.querySelectorAll(".letter");
-    answer.forEach(char => {
-        if(char.value == letter){
-            char.classList.remove("invisible")            
-        }
-    });
-}
-
-keys.forEach(key => {
-   key.addEventListener("click", ()=>{
-    checkWord(key.textContent)
+  "Lagos state",
+  "Aso Rock",
+  "Niagra Falls",
+];
+const selectWord = ()=>{
+    wordPick = Math.floor(Math.random()*words.length);
+    word = words[wordPick];
+    letters = word.toUpperCase().split("");
     
-   }) 
+    displayWord();
+}
+
+
+
+
+const displayWord = () => {
+  letters.forEach((char) => {
+    if (char == " ") {
+      let space = document.createElement("div");
+      space.classList.add("space");
+      screen.appendChild(space);
+    } else {
+      let letter = document.createElement("input");
+      letter.type = "text";
+      letter.classList.add("letter");
+      letter.value = char;
+      letter.classList.add("invisible");
+      let letterBox = document.createElement("div");
+      letterBox.classList.add("letter-box");
+      letterBox.appendChild(letter);
+      screen.appendChild(letterBox);
+    }
+  });
+};
+
+const checkWord = (letter) => {
+  let answer = document.querySelectorAll(".letter");
+  answer.forEach((char) => {
+    if (char.value == letter) {
+      char.classList.remove("invisible");
+    }
+  });
+};
+
+keys.forEach((key) => {
+  key.addEventListener("click", () => {
+    checkWord(key.textContent);
+  });
 });
 
 displayWord();
+
+newGameBtn.addEventListener("click", ()=>{
+    while(screen.hasChildNodes()){
+        screen.removeChild(screen.firstChild);
+    }
+    
+    selectWord()
+})
+
